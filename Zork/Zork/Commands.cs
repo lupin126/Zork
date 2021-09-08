b109cd5e0e13f41218b69dabcc3dd641e3eadb37
lupin126 <47069534+lupin126@users.noreply.github.com>
2021-09-08 00:31:01 -0400

    Zork 1.2

---
+++ Zork/Zork/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork
+{
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+}
