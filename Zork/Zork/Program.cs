4ac5cb5e6f23145e4789588bfed802ecd2e47133
lupin126 <47069534+lupin126@users.noreply.github.com>
2021-09-15 18:51:44 -0400

    Zork 2.1

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -4,41 +4,82 @@ namespace Zork
 {
 	class Program
 	{
+        private static string Location
+        {
+            get
+            {
+                return Rooms[LocationColumn];
+            }
+        }
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Welcome to Zork!");
 
 			while (true)
             {
-				string inputString = Console.ReadLine();
-				Commands command = ToCommand(inputString.Trim());
+                Console.Write($"{Location}\n> ");
+				Commands command = ToCommand(Console.ReadLine().Trim());
+                if(command == Commands.QUIT)
+                {
+                    break;
+                }
 
 				string outputString;
-				switch (command)
+                switch (command)
                 {
-					case Commands.QUIT:
-						outputString = "Thank you for playing.";
-						break;
-					case Commands.LOOK:
-						outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the front door.";
-						break;
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the front door.";
+                        break;
 
-					case Commands.NORTH:
-					case Commands.SOUTH:
-					case Commands.EAST:
-					case Commands.WEST:
-						outputString = $"You moved {command}.";
-						break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = Move(command) ? $"You moved {command}." : "The way is shut!";
+                        break;
 
 					default:
-						outputString = "Unrecognized command.";
+						outputString = "Unknown command.";
 						break;
 				}
 
 				Console.WriteLine(outputString);
             }
+
+            Console.WriteLine("Finished.");
 		}
 
+        private static bool Move(Commands command)
+        {
+            bool didMove = false;
+
+            switch (command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    break;
+
+                case Commands.EAST when LocationColumn < Rooms.Length - 1:
+                    LocationColumn++;
+                    didMove = true;
+                    break;
+
+                case Commands.WEST when LocationColumn > 0:
+                    LocationColumn--;
+                    didMove = true;
+                    break;
+            }
+
+            return didMove;
+        }
+
 		private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
+        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int LocationColumn = 1;
 	}
 }
