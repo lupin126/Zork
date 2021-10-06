943a0adf1feb7ba2ec1094a8b3e2928af6e6185d
lupin126 <47069534+lupin126@users.noreply.github.com>
2021-10-06 19:00:18 -0400

    Zork 4.2 Fix

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -19,14 +20,13 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            const string roomDescriptionsFilename = "Rooms.txt";
+            const string roomsFilename = "Rooms.json";
 
-            InitializeRoomDescriptions(roomDescriptionsFilename);
+            InitializeRooms(roomsFilename);
 
             Room previousLocation = null;
 
             Commands command = Commands.UNKNOWN;
-
             while (command != Commands.QUIT)
             {
                 Console.WriteLine(Location.Name);
@@ -101,43 +101,12 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        private static void InitializeRoomDescriptions(string roomDescriptionsFilename)
+        private static void InitializeRooms(string roomsFilename)
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
-            {
-                roomMap[room.Name] = room;
-            }
-
-            string[] lines = File.ReadAllLines(roomDescriptionsFilename);
-            foreach (string line in lines)
-            {
-                const string delimiter = "##";
-                const int expectedFieldCount = 2;
-
-                string[] fields = line.Split(delimiter);
-                if (fields.Length != expectedFieldCount)
-                {
-                    throw new Exception("Invalid Record");
-                }
-
-                (string name, string description) = (fields[(int)Fields.Name], fields[(int)Fields.Description]);
-                roomMap[name].Description = description;
-            }
+            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
         }
 
-        private static Room[,] Rooms =
-        {
-            {new Room("Dense Woods"),   new Room("North of House"),   new Room("Clearing") },
-            {new Room("Forest"),        new Room("West of House"),    new Room("Behind House") },
-            {new Room("Rocky Trail"),   new Room("South of House"),   new Room("Canyon View") }
-        };
-
-        private enum Fields
-        {
-            Name = 0,
-            Description = 1
-        }
+        private static Room[,] Rooms;
 
         private static int LocationColumn = 1;
         private static int LocationRow = 1;
